845f5e11aac8c6fdda29fe75030b992576776da8
Gabriella_Akpo <ella9894@gmail.com>
2022-04-05 19:10:11 -0400

    start aand end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,7 @@ var fight = function (enemyName) {
         if (promptFight === "skip" || promptFight === "SKIP") {
             //confimr player wants to skip
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-            //if yes (true).leave fight
+            //if yes (true),leave fight
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 //subtract money from player for skipping
